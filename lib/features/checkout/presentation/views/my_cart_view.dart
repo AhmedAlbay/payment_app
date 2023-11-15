551b86938f21046746315545b52e2eac192b0ed6
Your Name <you@example.com>
2023-11-15 15:29:59 +0200

    Add basket Image

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:payment_app/core/utils/styles.dart';
+import 'package:payment_app/features/checkout/presentation/views/widgets/my_cart_view_body.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -8,9 +10,15 @@ class MyCartView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Center(child: SvgPicture.asset("assets/images/Arrow 1.svg")),
+        centerTitle: true,
+        elevation: 0,
+
+        backgroundColor: Colors.transparent,
         title: const Text('My Cart',
             textAlign: TextAlign.center, style: Styles.style25),
       ),
+      body:const  MyCartViewBody(),
     );
   }
 }
