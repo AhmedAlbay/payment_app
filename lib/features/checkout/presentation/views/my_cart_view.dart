389fe0fb56f6de600d39a6bd513452d10fea109b
Your Name <you@example.com>
2023-11-15 14:19:26 +0200

    Add Styles to App

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/core/utils/styles.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(title: const Text(
-          'My Cart',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w500,
-            height: 0,
-          ),
-        ),),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Cart',
+            textAlign: TextAlign.center, style: Styles.style25),
+      ),
     );
   }
 }
