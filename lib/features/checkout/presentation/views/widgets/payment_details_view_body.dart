7a4e63908e837305c996a2dc6426767d035e7573
Your Name <you@example.com>
2023-11-28 23:43:37 +0200

    create a creditcartwidget and creditcardform

--- lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/features/checkout/presentation/views/widgets/custom_credit_card.dart';
 import 'package:payment_app/features/checkout/presentation/views/widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
@@ -6,12 +7,18 @@ class PaymentDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Padding(
-      padding:EdgeInsets.symmetric(horizontal: 20.0),
-      child: Column(
-        children: [
-          PaymentMethodsListView(),
-        ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            PaymentMethodsListView(),
+            SizedBox(
+              height: 16,
+            ),
+            CustomCreditCard(),
+          ],
+        ),
       ),
     );
   }
