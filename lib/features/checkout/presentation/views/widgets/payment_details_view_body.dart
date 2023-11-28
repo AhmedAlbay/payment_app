4de7467a295c442b889359bd121749656f08c9f9
Your Name <you@example.com>
2023-11-28 23:06:56 +0200

    make a second screen

--- lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/payment_details_view_body.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/features/checkout/presentation/views/widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
   const PaymentDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  const Padding(
+      padding:EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        children: [
+          PaymentMethodsListView(),
+        ],
+      ),
+    );
   }
 }
