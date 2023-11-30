bdffed2d9183dea801a9579110e7bf2cb7884b1f
Your Name <you@example.com>
2023-11-30 20:29:20 +0200

    make a circleavatar check

--- lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -6,8 +6,8 @@ class ThankYouViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Stack(children: [
+      padding: const EdgeInsets.all(20),
+      child: Stack(clipBehavior: Clip.none, children: [
         Container(
           decoration: ShapeDecoration(
             color: const Color(0xFFD9D9D9),
@@ -28,6 +28,23 @@ class ThankYouViewBody extends StatelessWidget {
             child: const CircleAvatar(
               backgroundColor: Colors.white,
             )),
+        const Positioned(
+          top: -50,
+          left: 0,
+          right: 0,
+          child: CircleAvatar(
+            radius: 50,
+            backgroundColor: Color(0xFFD9D9D9),
+            child: CircleAvatar(
+              radius: 40,
+              backgroundColor: Color(0xff34A853),
+              child: Icon(
+                Icons.check,
+                size: 50,
+              ),
+            ),
+          ),
+        )
       ]),
     );
   }
